a0e65e9e8a2a74ae2249c2e00e56c0c10dd8b665
PGreatness <ahasan1@stuy.edu>
2019-05-13 15:59:14 -0400

    modify for register page

--- app.py
+++ app.py
@@ -5,14 +5,17 @@ app = Flask("__main__")
 
 @app.route('/')
 def home():
-    return render_template('base.html')
+    return render_template('home.html')
 
-@app.route('/register', methods=['POST'])
+@app.route('/register')
 def register():
+    return render_template('register.html')
+@app.route('/auth', methods=['POST'])
+def auth():
     user = request.form.get("user")
     paswrd = request.form.get('pass')
     db.register(user, paswrd)
-    return render_template('base.html')
+    return redirect(url_for('home'))
 
 if __name__ == "__main__":
     app.debug = True
