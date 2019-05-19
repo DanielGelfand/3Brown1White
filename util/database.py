6ebe26a71841df5da40b5ff3bea87c8755032557
Ahnaf Hasan <ahasan1@stuy.edu>
2019-05-19 15:38:06 -0400

    Fix database commands

--- util/database.py
+++ util/database.py
@@ -5,6 +5,7 @@ LOGINS = "LOGINS"
 FINANCE = "FINANCES"
 LOSE = "SACRIFICES"
 GOALS = "GOALS"
+IMAGES = "IMAGES"
 
 print('Database works')
 DIR=os.path.dirname(__file__)
@@ -14,9 +15,10 @@ print(DIR)
 CONNECT = sqlite3.connect(DIR)
 CURSOR = CONNECT.cursor()
 CURSOR.execute(f"CREATE TABLE IF NOT EXISTS {LOGINS}(user TEXT, pass TEXT, id INTEGER)")
-CURSOR.execute(f"CREATE TABLE IF NOT EXISTS {FINANCE}(current_balance REAL, monthly_costs REAL, income INTEGER, id INTEGER)")
+CURSOR.execute(f"CREATE TABLE IF NOT EXISTS {FINANCE}(current_balance REAL, monthly_costs REAL, income REAL, expenses REAL, id INTEGER)")
 CURSOR.execute(f"CREATE TABLE IF NOT EXISTS {LOSE}(suggested0 INTEGER, suggested1 INTEGER, suggested2 INTEGER, custom REAL, id INTEGER)")
 CURSOR.execute(f"CREATE TABLE IF NOT EXISTS {GOALS}(name TEXT, cost REAL, id INTEGER)")
+CURSOR.execute(f"CREATE TABLE IF NOT EXISTS {IMAGES}(goal TEXT, goal_alt TEXT, id INTEGER)") # can be expanded later on
 CONNECT.commit()
 CONNECT.close()
 
@@ -70,16 +72,23 @@ def search_goal_list(*args):
     query_list = CURSOR.fetchall()
     return [ x for x in query_list for a in args if a in x ]
 
-def add_finances(balance, cost, income, id_num):
+def search_image_list(*args):
+    CONNECT = sqlite3.connect(DIR)
+    CURSOR = CONNECT.cursor()
+    CURSOR.execute(f"SELECT * FROM {IMAGES}")
+    query_list = CURSOR.fetchall()
+    return [ x for x in query_list for a in args if a in x ]
+
+def add_finances(balance, cost, income, expenses, id_num):
     CONNECT = sqlite3.connect(DIR)
     CURSOR = CONNECT.cursor()
     CURSOR.execute(f"SELECT * FROM {FINANCE}")
     finance_list = CURSOR.fetchall()
-    user_ids = [ x[3] for x in finance_list ]
+    user_ids = [ x[-1] for x in finance_list ]
     if id_num in user_ids:
-        CURSOR.execute(f"UPDATE {FINANCE} SET current_balance = {balance}, monthly_costs = {cost}, income = {income} WHERE id = {id_num}")
+        CURSOR.execute(f"UPDATE {FINANCE} SET current_balance = \"{balance}\", monthly_costs = \"{cost}\", income = \"{income}\", expenses = {expenses} WHERE id = \"{id_num}\"")
     else:
-        CURSOR.execute(f"INSERT INTO {FINANCE} VALUES(\"{balance}\", \"{cost}\", \"{income}\", \"{id_num}\")")
+        CURSOR.execute(f"INSERT INTO {FINANCE} VALUES(\"{balance}\", \"{cost}\", \"{income}\", \"{expenses}\", \"{id_num}\")")
     CONNECT.commit()
     CONNECT.close()
     return id_num
@@ -89,11 +98,29 @@ def add_goals(name, price, id_num):
     CURSOR = CONNECT.cursor()
     CURSOR.execute(f"SELECT * FROM {GOALS}")
     goal_list = CURSOR.fetchall()
-    user_ids = [ x[2] for x in goal_list ]
+    user_ids = [ x[-1] for x in goal_list ]
+    print(user_ids)
+    print(id_num)
     if id_num in user_ids:
-        CURSOR.execute(f"UPDATE {GOALS} SET name = {name}, cost = {price} WHERE id = {id_num}")
+        print("ID is in user_id")
+        CURSOR.execute(f"UPDATE {GOALS} SET name = \"{name}\", cost = \"{price}\" WHERE id = \"{id_num}\"")
     else:
-        CURSOR.execute(f"INSERT INTO {GOALS} VALUES({name}, {price}, {id_num})")
+        CURSOR.execute(f"INSERT INTO {GOALS} VALUES(\"{name}\", \"{price}\", \"{id_num}\")")
+    CONNECT.commit()
+    CONNECT.close()
+    return id_num
+
+def add_images(goal, goal_alt, id_num):
+    CONNECT = sqlite3.connect(DIR)
+    CURSOR = CONNECT.cursor()
+    CURSOR.execute(f"SELECT * FROM {IMAGES}")
+    image_list = CURSOR.fetchall()
+    user_ids = [ x[-1] for x in image_list ]
+    if id_num in user_ids:
+        print("id in user ids")
+        CURSOR.execute(f"UPDATE {IMAGES} SET goal = \"{goal}\", goal_alt = \"{goal_alt}\" WHERE id = \"{id_num}\"")
+    else:
+        CURSOR.execute(f"INSERT INTO {IMAGES} VALUES(\"{goal}\", \"{goal_alt}\", \"{id_num}\")")
     CONNECT.commit()
     CONNECT.close()
     return id_num
